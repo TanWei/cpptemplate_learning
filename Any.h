2d97f072115edfa7c7f253aff43de3c10c46c611
Runaway <dcsvgwudi@gmail.com>
2022-01-12 13:39:58 +0800

    any初步实现

--- Any.h
+++ Any.h
@@ -1,12 +1,22 @@
+#ifndef __ANY_H__
+#define __ANY_H__
+
 #include <typeindex>
 #include <memory>
+using namespace std;
+namespace template_learning {
+
 class any {
-    // any():m_tpIdx {}
-    // explixt any(any& that)
+public:
+    any(void):m_tpIdx(std::type_index(typeid(void))) {}
+    any(any& that) : m_ptr(that.clone()), m_tpIdx(that.m_tpIdx) {}
+    any(any&& that) : m_ptr(std::move(that.m_ptr)), m_tpIdx(that.m_tpIdx) {}
+    template<class U, 
+        class = typename std::enable_if<!is_same<typename std::decay<U>::type, any>::value,U>::type>
+    any(U&& value) : m_ptr(new derived<typename decay<U>::type>(forward<U>(value))), m_tpIdx(type_index(typeid(typename decay<U>::type))) {}
 private:
     class base;
-    typedef unique_ptr<base> basePtr;
-    //using basePtr = unique_ptr<base>;
+    using basePtr = std::unique_ptr<base>;
     class base {
     public:
         virtual ~base() {}
@@ -19,12 +29,23 @@ private:
         template<class U>
         derived(U && value) : m_value(forward<U>(value)) {}
         basePtr clone() const {
-             return BasePtr(new derived<T>(m_value));
+             return basePtr(new derived<T>(m_value));
         }
 
         T m_value;
     };
 
+    basePtr clone() const
+	{
+		if (m_ptr != nullptr)
+			return m_ptr->clone();
+		return nullptr;
+	}
+
 private:
     std::type_index m_tpIdx;
+    basePtr m_ptr;
 };
+}
+
+#endif // __ANY_H__
--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include "Any.h"
+#include <vector>
+#include <unordered_map>
+
+class A {
+private:
+    int m_{99};
+};
 
 int main() {
-
+    unordered_multimap< int, template_learning::any > v;
+    template_learning::any n(9);
+    v.insert({1, n});
+    // vector<template_learning::Any> v;
+    // template_learning::Any n(9);
+    // v.push_back(n);
+    // template_learning::Any a(A());
+    // v.push_back(a);
+    return 0;
 }
